e111c408bf758e23b23dbb15dc37db0b3c83cbf7
JasonLinn <cheng11220@gmail.com>
2017-08-26 12:10:18 +0800

    route finished

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const reload = require('reload');
 const app = express();
 const routes = require('./routes');
 var config = require('config-lite')(__dirname);
-const pkg = require('./package');
+const pkg = require('./package'); //package.json設定
 var urlencode = require('urlencode');
 var bodyParser = require('body-parser')
 const multer = require('multer');
@@ -20,7 +20,18 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //   uploadDir: path.join(__dirname, 'public/img'),// 上传文件目录
 //   keepExtensions: true// 保留后缀
 // }));
-
+// app.use(session({
+//     name: config.session.key,// 设置 cookie 中保存 session id 的字段名称
+//     secret: config.session.secret,// 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+//     resave: true,// 强制更新 session
+//     saveUninitialized: false,// 设置为 false，强制创建一个 session，即使用户未登录
+//     cookie: {
+//       maxAge: config.session.maxAge// 过期时间，过期后 cookie 中的 session id 自动删除
+//     },
+//     store: new MongoStore({// 将 session 存储到 mongodb
+//       url: 'mongodb://newjasonsblog:newjasonsblog@ds153352.mlab.com:53352/heroku_q1pm12v3'// mongodb 地址
+//     })
+//   }));
 
 routes(app);
 // app.use('/',function() {
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 module.exports = function (app) {
     app.get('/',function (req,res) {res.render('index')})
     app.get('/about',function (req,res) {res.render('about')})
-    app.get('/home',function (req,res) {res.render('home')})
+    app.get('/rooms',function (req,res) {res.render('rooms')})
     app.get('/news',function (req,res) {res.render('news')})
     app.use('/login',require('./login'));
     app.use('/cms_news',require('./news'));
