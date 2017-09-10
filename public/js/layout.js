b1f578c65f38bbccd31c179fe720729aa4d949e9
Leo Su <leo.suoxxo@gmail.com>
2017-09-10 22:29:01 +0800

    update jQuery scrollbar  and fixed index background-image

--- public/js/layout.js
+++ public/js/layout.js
@@ -7,12 +7,22 @@ $('#menuHam').on('change',function(){
 
 $(window).on('scrollstart', function() {
     $('header#header').stop(true, false).animate({ 'top': '-100px', 'opacity': 0 }, 100);
-    // alert(123)
 });
 
 $(window).bind('scrollstop', function(e) {
     $('header#header').stop(true, false).animate({ 'top': '0px', 'opacity': 1 }, 500);
 });
+
+
+
+/*************/
+/****index****/
+/*************/
+
+$('#page-index .newsbox .contentbox').mCustomScrollbar({
+  axis: "y"
+})
+
 
 
 /*************/
@@ -25,6 +35,7 @@ $('#page-news .morebtn').click(function(){
   let content = $(this).parent().children("div:nth-child(3)").text();
   let imgURL = $(this).parent().children("input").val();
   console.log(title,date,imgURL)
+  fixedBody();
   $('#popNewsDetail').show();
   $('#popNewsDetail .title').text(title);
   $('#popNewsDetail .date').text(date);
@@ -34,10 +45,16 @@ $('#page-news .morebtn').click(function(){
 })
 
 $('#popNewsDetail .popbg').click(function(){
+  looseBody();
   $('#popNewsDetail').hide();
 })
 $('#popNewsDetail .closebtn').click(function(){
+  looseBody();
   $('#popNewsDetail').hide();
+})
+
+$('#popNewsDetail .popcontent').mCustomScrollbar({
+  axis: "y"
 })
 
 /*************/
@@ -45,13 +62,38 @@ $('#popNewsDetail .closebtn').click(function(){
 /*************/
 
 $('#page-tours .morebtn').click(function(){
+  fixedBody();
   $('#popTourDetail').show();
 })
 
 
 $('#popTourDetail .popbg').click(function(){
   $('#popTourDetail').hide();
+  looseBody();
 })
 $('#popTourDetail .closebtn').click(function(){
   $('#popTourDetail').hide();
+  looseBody();
 })
+
+$('#popTourDetail .popcontent').mCustomScrollbar({
+  axis: "y"
+})
+
+
+
+//*******************/
+//******固定背景*****/
+//******************/
+function fixedBody() {
+  var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+  document.body.style.cssText += 'position:fixed;top:-' + scrollTop + 'px;';
+}
+
+function looseBody() {
+  var body = document.body;
+  body.style.position = 'relative';
+  var top = body.style.top;
+  document.body.scrollTop = document.documentElement.scrollTop = -parseInt(top);
+  body.style.top = '';
+}
